51607a21d1d28a0c81f5d6c0cb185788a88ca2e5
dbrown20 <dbrown20@uvm.edu>
2022-10-19 21:23:27 -0400

    button takes you to a second view

--- PocketPantry/ContentView.swift
+++ PocketPantry/ContentView.swift
@@ -2,44 +2,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-
+        
         VStack(alignment: .center) {
             HStack(alignment: .center){
-              Text("Pocket Pantry")
+                Text("Pocket Pantry")
             }
             HStack{
-                NavigationView {
-                    Button(action: {}){
+                
+                NavigationView{
+                    NavigationLink(destination: pantryView()){
                         Text("View Pantry")
-                        .bold()
-                        .font(Font.custom("Helvetica Neue", size: 14.0))
-                        .padding(20)
-                        .foregroundColor(Color.white)
-                        .background(Color.black)
-                        .cornerRadius(12)
-                        }
-                        .toolbar {
-                            ToolbarItemGroup(placement: .bottomBar) {
-                                Button("Home") {
-                                    print("Pressed")
-                                }
-
-                                Spacer()
-
-                                Button("Settings") {
-                                    print("Pressed")
+                            .bold()
+                            .font(Font.custom("Helvetica Neue", size: 14.0))
+                            .padding(20)
+                            .foregroundColor(Color.white)
+                            .background(Color.black)
+                            .cornerRadius(12)
+                            .toolbar {
+                                ToolbarItemGroup(placement: .bottomBar) {
+                                    Button("Home") {
+                                        print("Pressed")
+                                    }
+                                    
+                                    Spacer()
+                                    
+                                    Button("Settings") {
+                                        print("Pressed")
+                                    }
                                 }
                             }
-                        }
+                    }
                 }
-            }
-            Spacer()
-        }//.background(Color.mint)
+                Spacer()
+            }//.background(Color.mint)
+        }
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
---
+++ PocketPantry/pantryView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct pantryView: View {
+    var body: some View {
+        Text("Wow look at all that food")
+    }
+}
+
+struct pantryView_Previews: PreviewProvider {
+    static var previews: some View {
+        pantryView()
+    }
+}
